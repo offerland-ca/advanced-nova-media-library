473c3e22145e5cf8d357408ea35524153874b1e7
Parham Alvani <parham.alvani@gmail.com>
2024-09-05 16:37:34 +0000

    feat: generate new version

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,24 +1,34 @@
-const mix = require('laravel-mix');
-const path = require('path');
+const mix = require("laravel-mix");
+const path = require("path");
 
-mix.setPublicPath('dist')
-  .js('resources/js/field.js', 'js').vue({version: 3})
+mix
+  .setPublicPath("dist")
+  .js("resources/js/field.js", "js")
+  .vue({ version: 3 })
   .webpackConfig({
     externals: {
-      vue: 'Vue',
+      vue: "Vue",
     },
     output: {
-      uniqueName: 'vendor/package',
-    }
+      uniqueName: "vendor/package",
+    },
   })
   .alias({
-    'laravel-nova': path.join(__dirname, '../../laravel/nova/resources/js/mixins/packages.js'),
-    'axios': path.join(__dirname, 'node_modules/axios'),
-    'lodash': path.join(__dirname, 'node_modules/lodash'),
-    'form-backend-validation': path.join(__dirname, 'node_modules/form-backend-validation'),
-    '@babel/plugin-transform-runtime': path.join(__dirname, 'node_modules/@babel/plugin-transform-runtime'),
-    '@babel/runtime': path.join(__dirname, 'node_modules/@babel/runtime'),
-    'vuex': path.join(__dirname, 'node_modules/vuex'),
-    '@inertiajs/inertia': path.join(__dirname, 'node_modules/@inertiajs/inertia'),
+    axios: path.join(__dirname, "node_modules/axios"),
+    lodash: path.join(__dirname, "node_modules/lodash"),
+    "form-backend-validation": path.join(
+      __dirname,
+      "node_modules/form-backend-validation",
+    ),
+    "@babel/plugin-transform-runtime": path.join(
+      __dirname,
+      "node_modules/@babel/plugin-transform-runtime",
+    ),
+    "@babel/runtime": path.join(__dirname, "node_modules/@babel/runtime"),
+    vuex: path.join(__dirname, "node_modules/vuex"),
+    "@inertiajs/inertia": path.join(
+      __dirname,
+      "node_modules/@inertiajs/inertia",
+    ),
   })
-  .sass('resources/sass/field.scss', 'css')
+  .sass("resources/sass/field.scss", "css");
