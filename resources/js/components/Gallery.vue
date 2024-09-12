b5d18f1b2b688a2e9bcc66d25c09d854dbea6dc9
Parham Alvani <parham.alvani@gmail.com>
2024-09-12 06:41:32 +0000

    chore: remove button because it does not work

--- resources/js/components/Gallery.vue
+++ resources/js/components/Gallery.vue
@@ -52,6 +52,7 @@
     <br style="clear: both" />
 
     <span v-if="editable" class="">
+      <!--
       <label :for="`__media__${field.attribute}`" class="">
         <DefaultButton type="button" @click.prevent="focusFileInput">
           <template v-if="uploading"
@@ -60,6 +61,7 @@
           <template v-else>{{ label }}</template>
         </DefaultButton>
       </label>
+      -->
       <input
         :id="`__media__${field.attribute}`"
         :multiple="multiple"
