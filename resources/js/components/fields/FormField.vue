6e3c40e0ca440f0eb4ddc15f5930558c45fc9ff6
Parham Alvani <parham.alvani@gmail.com>
2024-09-12 06:21:03 +0000

    fix: format vue files

--- resources/js/components/fields/FormField.vue
+++ resources/js/components/fields/FormField.vue
@@ -1,16 +1,40 @@
 <template>
-  <component :is="field.fullSize ? 'FullWidthField' : 'DefaultField'" :field="field" :errors="errors" :show-help-text="showHelpText">
+  <component
+    :is="field.fullSize ? 'FullWidthField' : 'DefaultField'"
+    :field="field"
+    :errors="errors"
+    :show-help-text="showHelpText"
+  >
     <template #field>
-      <div :class="{'px-8 pt-6': field.fullSize}">
-        <gallery slot="value" ref="gallery" v-if="hasSetInitialValue"
-                 v-model="value" :editable="!field.readonly" :removable="field.removable" custom-properties :field="field" :multiple="field.multiple" :uploads-to-vapor="field.uploadsToVapor"
-                 :has-error="hasError" :first-error="firstError"/>
+      <div :class="{ 'px-8 pt-6': field.fullSize }">
+        <gallery
+          slot="value"
+          ref="gallery"
+          v-if="hasSetInitialValue"
+          v-model="value"
+          :editable="!field.readonly"
+          :removable="field.removable"
+          custom-properties
+          :field="field"
+          :multiple="field.multiple"
+          :uploads-to-vapor="field.uploadsToVapor"
+          :has-error="hasError"
+          :first-error="firstError"
+        />
 
         <div v-if="field.existingMedia">
-          <OutlineButton type="button" class="mt-2" @click.prevent="existingMediaOpen = true">
+          <OutlineButton
+            type="button"
+            class="mt-2"
+            @click.prevent="existingMediaOpen = true"
+          >
             {{ openExistingMediaLabel }}
           </OutlineButton>
-          <existing-media :open="existingMediaOpen" @close="existingMediaOpen = false" @select="addExistingItem"/>
+          <existing-media
+            :open="existingMediaOpen"
+            @close="existingMediaOpen = false"
+            @select="addExistingItem"
+          />
         </div>
         <help-text
           class="error-text mt-2 text-danger"
@@ -24,37 +48,37 @@
 </template>
 
 <script>
-import {FormField, HandlesValidationErrors} from 'laravel-nova';
-import Gallery from '../Gallery';
-import FullWidthField from '../FullWidthField';
-import ExistingMedia from '../ExistingMedia';
-import objectToFormData from 'object-to-formdata';
-import get from 'lodash/get';
+import { FormField, HandlesValidationErrors } from "laravel-nova";
+import Gallery from "../Gallery";
+import FullWidthField from "../FullWidthField";
+import ExistingMedia from "../ExistingMedia";
+import objectToFormData from "object-to-formdata";
+import get from "lodash/get";
 
 export default {
   mixins: [FormField, HandlesValidationErrors],
   components: {
     Gallery,
     FullWidthField,
-    ExistingMedia
+    ExistingMedia,
   },
-  props: ['resourceName', 'resourceId', 'field'],
+  props: ["resourceName", "resourceId", "field"],
   data() {
     return {
       hasSetInitialValue: false,
       existingMediaOpen: false,
-    }
+    };
   },
   computed: {
     openExistingMediaLabel() {
-      const type = this.field.type === 'media' ? 'Media' : 'File';
+      const type = this.field.type === "media" ? "Media" : "File";
 
       if (this.field.multiple || this.value.length === 0) {
         return this.__(`Add Existing ${type}`);
       }
 
       return this.__(`Use Existing ${type}`);
-    }
+    },
   },
   methods: {
     /*
@@ -83,49 +107,79 @@ export default {
           if (file.isVaporUpload) {
             // In case of Vapor upload, do not send the file's binary data over the wire.
             // The file can already be found in the bucket.
-            formData.append(`__media__[${field}][${index}][is_vapor_upload]`, true);
-            formData.append(`__media__[${field}][${index}][key]`, file.vaporFile.key);
-            formData.append(`__media__[${field}][${index}][uuid]`, file.vaporFile.uuid);
-            formData.append(`__media__[${field}][${index}][file_name]`, file.vaporFile.filename);
-            formData.append(`__media__[${field}][${index}][file_size]`, file.vaporFile.file_size);
-            formData.append(`__media__[${field}][${index}][mime_type]`, file.vaporFile.mime_type);
+            formData.append(
+              `__media__[${field}][${index}][is_vapor_upload]`,
+              true,
+            );
+            formData.append(
+              `__media__[${field}][${index}][key]`,
+              file.vaporFile.key,
+            );
+            formData.append(
+              `__media__[${field}][${index}][uuid]`,
+              file.vaporFile.uuid,
+            );
+            formData.append(
+              `__media__[${field}][${index}][file_name]`,
+              file.vaporFile.filename,
+            );
+            formData.append(
+              `__media__[${field}][${index}][file_size]`,
+              file.vaporFile.file_size,
+            );
+            formData.append(
+              `__media__[${field}][${index}][mime_type]`,
+              file.vaporFile.mime_type,
+            );
           } else {
-            formData.append(`__media__[${field}][${index}]`, file.file, file.name);
+            formData.append(
+              `__media__[${field}][${index}]`,
+              file.file,
+              file.name,
+            );
           }
         } else {
           formData.append(`__media__[${field}][${index}]`, file.id);
         }
 
-        objectToFormData({
-          [`__media-custom-properties__[${field}][${index}]`]: this.getImageCustomProperties(file)
-        }, {}, formData);
+        objectToFormData(
+          {
+            [`__media-custom-properties__[${field}][${index}]`]:
+              this.getImageCustomProperties(file),
+          },
+          {},
+          formData,
+        );
       });
     },
 
     getImageCustomProperties(image) {
-      return (this.field.customPropertiesFields || []).reduce((properties, {attribute: property}) => {
-        properties[property] = get(image, `custom_properties.${property}`);
+      return (this.field.customPropertiesFields || []).reduce(
+        (properties, { attribute: property }) => {
+          properties[property] = get(image, `custom_properties.${property}`);
 
-        // Fixes checkbox problem
-        if (properties[property] === true) {
-          properties[property] = 1;
-        }
+          // Fixes checkbox problem
+          if (properties[property] === true) {
+            properties[property] = 1;
+          }
 
-        return properties;
-      }, {})
+          return properties;
+        },
+        {},
+      );
     },
 
     /**
      * Update the field's internal value.
      */
     handleChange(value) {
-      this.value = value
+      this.value = value;
     },
 
     addExistingItem(item) {
       // Copy to trigger watcher to recognize differnece between new and old values
       // https://github.com/vuejs/vue/issues/2164
-      let copiedArray = this.value.slice(0)
+      let copiedArray = this.value.slice(0);
 
       if (!this.field.multiple) {
         copiedArray.splice(0, 1);
@@ -133,7 +187,7 @@ export default {
 
       copiedArray.push(item);
       this.value = copiedArray;
-    }
+    },
   },
 };
 </script>
--- resources/js/field.js
+++ resources/js/field.js
@@ -1,9 +1,9 @@
-import IndexField from './components/fields/IndexField';
-import DetailField from './components/fields/DetailField';
-import FormField from './components/fields/FormField';
+import IndexField from "./components/fields/IndexField";
+import DetailField from "./components/fields/DetailField";
+import FormField from "./components/fields/FormField";
 
-Nova.booting((app, store) => {
-  app.component('index-advanced-media-library-field', IndexField);
-  app.component('detail-advanced-media-library-field', DetailField);
-  app.component('form-advanced-media-library-field', FormField);
+Nova.booting((app, _) => {
+  app.component("index-advanced-media-library-field", IndexField);
+  app.component("detail-advanced-media-library-field", DetailField);
+  app.component("form-advanced-media-library-field", FormField);
 });
